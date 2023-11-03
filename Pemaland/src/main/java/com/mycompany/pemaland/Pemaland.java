2a6e4172f69337ecc5af8072361fc3220f694645
Muhammad Adhi Gozalt <gozalt@student.telkomuniversity.ac.id>
2023-11-03 12:25:11 +0700

    [1] Folder Pemaland

---
+++ Pemaland/src/main/java/com/mycompany/pemaland/Pemaland.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pemaland;
+
+/**
+ *
+ * @author muham
+ */
+public class Pemaland {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
